1412b3ade9407cc83194223e414050088ed5fbff
aziffarhan <aziffarhan1947@gmail.com>
2023-03-30 15:54:01 +0800

    Quiz 1

---
+++ quiz1.js
@@ -0,0 +1,38 @@
+// let message = "benr"
+// console.log(message, typeof message)
+
+// message = 123
+// console.log(message, typeof message)
+
+// let str = "1.258"
+// let num = 1
+// let total = parseFloat(str) + num
+// console.log(total, typeof total)
+
+// let data = ["BENR", 3523, true]
+// console.log(data[0], data[1], data[2])
+
+// let user = {
+//     name: "Azif",
+//     age: 23,
+//     faculty: "FKEKK"
+// }
+// console.log(user.name)
+// console.log(user.age)
+// console.log(user.faculty)
+
+let messages = [
+    {
+        name: "usr1",
+        date: "1/4/2023",
+        msg: "ayamguring",
+        like: 2000
+    },
+    {
+        name: "usr2",
+        data: "2/4/2023",
+        msg: "very good video",
+        like: 3000
+    },
+]
+console.log(messages[1].msg) //very good video
